824ce75c1efe1be8f4839b98d51ee0d616d5ca5a
Obedkov Vladislav <230765@astanait.edu.kz>
2023-11-15 00:27:48 +0600

    Updated schedule

--- src/utils/scheduleClasses.js
+++ src/utils/scheduleClasses.js
@@ -3,29 +3,14 @@ export const scheduleClasses = [
     name: "Monday",
     classes: [
       {
-        name: "Introduction to programming",
-        time: "8:00 - 8:50",
-        room: "C1.2.242K",
+        name: "Linear algebra",
+        time: "8:00 - 9:50",
+        room: "C1.1.252L",
       },
       {
-        name: "Cultural studies",
-        time: "09:00 - 09:50",
-        room: "C1.2.250L",
-      },
-      {
-        name: "Foreign Language",
-        time: "12:00 - 12:50",
-        room: "C1.1.355P",
-      },
-      {
-        name: "Information and Communication Technologies",
-        time: "17:00 - 17:50",
-        room: null,
-      },
-      {
-        nname: "Information and Communication Technologies",
-        time: "18:00 - 18:50",
-        room: null,
+        name: "Calculus 2",
+        time: "10:00 - 11:50",
+        room: "C1.1.328L",
       },
     ],
   },
@@ -33,14 +18,9 @@ export const scheduleClasses = [
     name: "Tuesday",
     classes: [
       {
-        name: "Cultural Studies",
-        time: "17:00 - 17:50",
-        room: null,
-      },
-      {
-        name: "Information and Communication Technologies",
-        time: "18:00 - 18:50",
-        room: null,
+        name: "OOP",
+        time: "12:00 - 13:50",
+        room: "C1.1.241K",
       },
     ],
   },
@@ -48,120 +28,40 @@ export const scheduleClasses = [
     name: "Wednesday",
     classes: [
       {
-        name: "Information and Communication Technologies",
+        name: "DBMS",
         time: "08:00 - 08:50",
+        room: "C1.1.348K",
+      },
+      {
+        name: "OOP",
+        time: "9:00 - 9:50",
         room: "C1.1.241K",
       },
       {
-        name: "Information and Communication Technologies",
-        time: "09:00 - 09:50",
-        room: "C1.1.241K",
-      },
-      {
-        name: "History of Kazakhstan",
-        time: "10:00 - 10:50",
-        room: "C1.2.246",
-      },
-      {
-        name: "History of Kazakhstan",
-        time: "11:00 - 11:50",
-        room: "C1.2.246",
-      },
-      {
-        name: "Introduction to Programming",
-        time: "17:00 - 17:50",
-        room: null,
-      },
-      {
-        name: "Introduction to Programming",
-        time: "18:00 - 18:50",
-        room: null,
-      },
+        name: "Linear algebra",
+        time: "10:00 - 11:50",
+        room: "C1.3.362",
+      }
     ],
   },
   {
     name: "Thursday",
     classes: [
       {
-        name: "Introduction to Programming",
-        time: "10:00 - 10:50",
-        room: "C1.2.242K",
+        name: "DBMS",
+        time: "8:00 - 9:50",
+        room: "C1.1.348K",
       },
       {
-        name: "Introduction to Programming",
-        time: "11:00 - 11:50",
-        room: "C1.2.242K",
-      },
-      {
-        name: "Calculus",
-        time: "12:00 - 12:50",
-        room: "C1.3.244",
-      },
-      {
-        name: "Calculus",
-        time: "13:00 - 13:50",
-        room: "C1.3.244",
-      },
-      {
-        name: "History of Kazakhstan",
-        time: "17:00 - 17:50",
-        room: null,
-      },
-      {
-        name: "History of Kazakhstan",
-        time: "18:00 - 18:50",
-        room: null,
-      },
-    ],
-  },
-  {
-    name: "Friday",
-    classes: [
-      {
-        name: "Foreign Language",
-        time: "09:00 - 09:50",
-        room: "C1.1.355P",
-      },
-      {
-        name: "Foreign Language",
+        name: "Sociology",
         time: "10:00 - 10:50",
         room: "C1.1.355P",
       },
       {
-        name: "Calculus",
-        time: "12:00 - 12:50",
-        room: "C1.1.328L",
-      },
-      {
-        name: "Calculus",
-        time: "13:00 - 13:50",
-        room: "C1.1.328L",
-      },
-      {
-        name: "PE",
-        time: "17:00 - 17:50",
-        room: "gym",
-      },
-      {
-        name: "PE",
-        time: "18:00 - 18:50",
-        room: "gym",
-      },
-    ],
-  },
-  {
-    name: "Saturday",
-    classes: [
-      {
-        name: "History of Kazahkstan",
-        time: "17:00 - 17:50",
-        room: null,
-      },
-      {
-        name: "Calculus",
-        time: "18:00 - 18:50",
-        room: null,
-      },
+        name: "Calculus 2",
+        time: "12:00 - 13:50",
+        room: "C1.1.324L",
+      }
     ],
   },
 ];
